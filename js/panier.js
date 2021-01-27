b0d66633f2c4b8d91ea7e31d516c8e606758583a
mylene_said@hotmail.fr <mylene_said@hotmail.fr>
2021-01-27 11:45:17 +0100

    validation des données et envoie au serveur

--- js/panier.js
+++ js/panier.js
@@ -25,100 +25,9 @@ panier.forEach((element) => {
 
   }); 
 
-////////////Formulaire de commande
-/*
-const formulaire = document.getElementById('formulaire')
-
-let form = document.createElement('form')
-form.setAttribute('class', 'form')
-form.setAttribute('method', 'post')
-//form.setAttribute('action', '')
-
-let divFirstName = document.createElement('div')
-divFirstName.setAttribute('class', 'form-group')
-let firstName = document.createElement('label') 
-firstName.innerHTML = 'Votre Nom de famille'
-firstName.setAttribute ('for', 'name')
-let inputFirstName = document.createElement('input')
-inputFirstName.setAttribute ('id', 'name')
-inputFirstName.setAttribute ('type', 'text')
-inputFirstName.setAttribute ('required', '') /// champs obligatoire 
-inputFirstName.setAttribute ('placeholder', 'Dumont')
-
-let divLastName = document.createElement('div')
-divLastName.setAttribute('class', 'form-group')
-let lastName = document.createElement('label') 
-lastName.innerHTML = 'Votre Prénom'
-lastName.setAttribute ('for', 'second-name')
-let inputLastName = document.createElement('input')
-inputLastName.setAttribute ('id', 'second-name')
-inputLastName.setAttribute ('type', 'text')
-inputLastName.setAttribute ('required', '')
-inputLastName.setAttribute ('placeholder', 'François')
-
-let divAdress = document.createElement('div')
-divAdress.setAttribute('class', 'form-group')
-let adress = document.createElement('label')
-adress.innerHTML = 'Votre Adresse postale'
-adress.setAttribute('for', 'adress')
-let inputAdress = document.createElement('input')
-inputAdress.setAttribute('id', 'adress')
-inputAdress.setAttribute('required', '')
-inputAdress.setAttribute('placeholder', '25 rue de baba')
-
-let divCity = document.createElement('div')
-divCity.setAttribute('class', 'form-group')
-let city = document.createElement('label')
-city.innerHTML = 'Votre Code postal/Ville'
-city.setAttribute('for', 'number')
-let inputCity = document.createElement('input')
-inputCity.setAttribute('id', 'number')
-inputCity.setAttribute('type', 'tel')
-inputCity.setAttribute('required', '')
-inputCity.setAttribute('placeholder', '97000 RAGIN')
+////////////Montant total panier
 
 
-let divMail = document.createElement('div')
-divMail.setAttribute('class', 'form-group')
-let mail = document.createElement('label')
-mail.innerHTML = 'Votre Numéro Adresse Mail'
-mail.setAttribute('for', 'mail')
-let inputMail = document.createElement('input')
-inputMail.setAttribute('id', 'mail')
-inputMail.setAttribute('type', 'email')
-inputMail.setAttribute('required', '')
-inputMail.setAttribute('placeholder', 'monadress@mail.fr')
-
-//// Boutton validation commande 
-let button = document.createElement('button')
-button.innerHTML = 'Valider la commande'
-button.setAttribute('type', 'submit')
-button.setAttribute('class', 'btn btn-primary')
-button.setAttribute('id', 'confirm-command')
-
-
-formulaire.appendChild(form)
-form.appendChild(divFirstName)
-divFirstName.appendChild(firstName)
-divFirstName.appendChild(inputFirstName)
-
-form.appendChild(divLastName)
-divLastName.appendChild(lastName)
-divLastName.appendChild(inputLastName)
-
-form.appendChild(divAdress)
-divAdress.appendChild(adress)
-divAdress.appendChild(inputAdress)
-
-form.appendChild(divCity)
-divCity.appendChild(city)
-divCity.appendChild(inputCity)
-
-form.appendChild(divMail)
-divMail.appendChild(mail)
-divMail.appendChild(inputMail)
-
-form.appendChild(button) */
 
 
 //// Validation du formulaire 
@@ -127,6 +36,7 @@ let formValid = document.getElementById('bouton-valider')
 let nom = document.getElementById('nom')
 let missNom = document.getElementById('missNom')
 let missPrenom = document.getElementById('missPrenom')
+//let prenomValid = /^[a-zA-ZéèîÏ]
 let missAdresse = document.getElementById('missAdresse')
 let missVille = document.getElementById('missVille')
 let missMail = document.getElementById('missMail')
@@ -144,7 +54,12 @@ function validation(event){
     event.preventDefault()
     missPrenom.textContent =  'Veuillez indiquer votre prénom svp'
     missPrenom.style.color = 'red'
-  }
+    
+  } /* else if (prenomValid.test(prenon.value)== false){
+    event.preventDefault()
+    missPrenom.textContent = 'Format incorrect'
+  }*/
+
    /// si le champ adresse est vide
    if(adresse.validity.valueMissing){
     event.preventDefault()
@@ -160,7 +75,22 @@ function validation(event){
    /// si le champ mail est vide
    if(mail.validity.valueMissing){
     event.preventDefault()
-    missMail.textContent =  'Veuillez indiquer votre mail de famille svp'
+    missMail.textContent =  'Veuillez indiquer votre mail svp'
     missMail.style.color = 'red'
   }
 }
+
+//// Envoi des données au serveur 
+
+fetch("http://localhost:3000/api/cameras/order",{
+  body:JSON.stringify(order),
+  method: "post",
+  headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+}) .then((response)=>{
+  response.json().then(function(data){
+    //enregistrement de "order" dans le localSorage
+    localStorage.setItem("order", JSON.stringify(data));
+    //ouverture de la page de confirmation
+    document.location = "commande.html";
+  })
+})
