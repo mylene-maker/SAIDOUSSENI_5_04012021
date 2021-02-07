7225109a873ad59dfe948c917bc18b7c7a708a59
mylene_said@hotmail.fr <mylene_said@hotmail.fr>
2021-02-07 12:45:53 +0100

    verification du formulaire

--- js/commande.js
+++ js/commande.js
@@ -3,7 +3,7 @@ let confirmation = document.getElementById("confirmation");
 let message = document.createElement("p");
 message.setAttribute("class", "Confirm-commande text-center font-weight-bold");
 message.innerHTML =
-  "Merci pour votre commande !<br> Nous espérons vous revoir très bientôt";
+"Merci pour votre commande !<br> Nous espérons vous revoir très bientôt";
 
 let button = document.createElement("a");
 button.setAttribute("class", "btn btn-primary btn-dark btnAcc");
--- js/panier.js
+++ js/panier.js
@@ -33,51 +33,62 @@ let total = document.getElementById("sum");
 total.innerHTML = totalTmp / 100 + ".00€";
 console.log(totalTmp);
 
-
-/// evenemnt click pour l'envoie du panier et formulaire
-
+//// Verification des champs
 
 let envoiForm = document.getElementById("bouton-valider");
 
-envoiForm.addEventListener("click", function () {
+envoiForm.addEventListener("click", function (e) {
+  let erreur;
+  let inputs = document.getElementsByTagName("input");
+  for (var i = 0; i < inputs.length; i++) {
+    if (!inputs[i].value) {
+      erreur = "Veuillez renseigner tout les champs !";
+    }
+  }
+  if (erreur) {
+    e.preventDefault();
+    document.getElementById("erreur").innerHTML = erreur;
+    return false;
+  } else {
+    let firstName = document.getElementById("nom").value;
+    let lastName = document.getElementById("prenom").value;
+    let address = document.getElementById("adresse").value;
+    let city = document.getElementById("ville").value;
+    let email = document.getElementById("mail").value;
 
-  let firstName = document.getElementById("nom").value;
-  let lastName = document.getElementById("prenom").value;
-  let address = document.getElementById("adresse").value;
-  let city = document.getElementById("ville").value;
-  let email = document.getElementById("mail").value;
+    // Objet contact à envoyer au serveur
 
-  // Objet contact à envoyer au serveur
+    const contact = {
+      firstName: firstName,
+      lastName: lastName,
+      address: address,
+      city: city,
+      email: email,
+    };
+    console.log(contact);
 
-  const contact = {
-    firstName: firstName,
-    lastName: lastName,
-    address: address,
-    city: city,
-    email: email,
-  };
-  console.log(contact);
+    // Tableau des id produit à envoyer au serveur
+    const products = panier.filter((item) => item._id).map((item) => item._id);
+    console.log(products);
 
-  // Tableau des id produit à envoyer au serveur
-  const products = panier.filter((item) => item._id).map((item) => item._id);
-  console.log(products);
+    //// Envoie au serveur de contact et product
 
+    fetch("http://localhost:3000/api/teddies/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        contact,
+        products,
+      }),
+    }).then((res) => res.json());
+    console.log(res);
+  }
 });
 
-/*/// Envoie au serveur
-
-fetch("http://localhost:3000/api/teddies/order", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    contact,
-    products,
-  }),
-}).then((res) => res.json()); */
-
-/*formValid.addEventListener("click", validation);
+/*let formValid = document.getElementById("bouton-valider")
+formValid.addEventListener("click", validation);
 function validation(event) {
   let codeRetour = true;
   /// si le champs Nom est vide
@@ -86,8 +97,38 @@ function validation(event) {
     missNom.textContent = "Veuillez indiquer votre nom de famille svp";
     missNom.style.color = "red";
     codeRetour = false;
+
+  } else{
+    let firstName = document.getElementById("nom").value;
+    let lastName = document.getElementById("prenom").value;
+    let address = document.getElementById("adresse").value;
+    let city = document.getElementById("ville").value;
+    let email = document.getElementById("mail").value;
+  
+    // Objet contact à envoyer au serveur
+  
+    const contact = {
+      firstName: firstName,
+      lastName: lastName,
+      address: address,
+      city: city,
+      email: email,
+    };
+    console.log(contact);
+
+     // Tableau des id produit à envoyer au serveur
+    const products = panier.filter((item) => item._id).map((item) => item._id);
+  console.log(products);
+  }*/
+
+/*// si le champ prénom est vide
+  /// si le champs Nom est vide
+  if (nom.validity.valueMissing) {
+    event.preventDefault();
+    missNom.textContent = "Veuillez indiquer votre nom de famille svp";
+    missNom.style.color = "red";
+    codeRetour = false;
   }
-  /// si le champ prénom est vide
   if (prenom.validity.valueMissing) {
     event.preventDefault();
     missPrenom.textContent = "Veuillez indiquer votre prénom svp";
@@ -130,5 +171,5 @@ function validation(event) {
     /// enregistrement dans le localStorage
     localStorage.setItem("contact", JSON.stringify(data));
     localStorage.setItem("products", JSON.stringify(data));
-  }
+  } 
 }*/
