076f11cd58b2228f2de41a4248397a1e8cb6953a
mylene_said@hotmail.fr <mylene_said@hotmail.fr>
2021-02-08 12:03:50 +0100

    bad request

--- js/panier.js
+++ js/panier.js
@@ -40,6 +40,21 @@ let envoiForm = document.getElementById("bouton-valider");
 envoiForm.addEventListener("click", function (e) {
   let erreur;
   let inputs = document.getElementsByTagName("input");
+  if(inputs['nom'] != ""){
+    erreur = ''
+  }
+  if(inputs['prenom'] != ""){
+    erreur = ''
+  }
+  if(inputs['adresse'] != ""){
+    erreur = ''
+  }
+  if(inputs['ville'] != ""){
+    erreur = ''
+  }
+  if(inputs['mail'] != ""){
+    erreur = ''
+  }
   for (var i = 0; i < inputs.length; i++) {
     if (!inputs[i].value) {
       erreur = "Veuillez renseigner tout les champs !";
@@ -73,9 +88,10 @@ envoiForm.addEventListener("click", function (e) {
 
     //// Envoie au serveur de contact et product
 
+    // envoi au serveur les objets contact et product
     fetch("http://localhost:3000/api/teddies/order", {
       method: "POST",
-      headers: {
+      header: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -83,93 +99,5 @@ envoiForm.addEventListener("click", function (e) {
         products,
       }),
     }).then((res) => res.json());
-    console.log(res);
   }
 });
-
-/*let formValid = document.getElementById("bouton-valider")
-formValid.addEventListener("click", validation);
-function validation(event) {
-  let codeRetour = true;
-  /// si le champs Nom est vide
-  if (nom.validity.valueMissing) {
-    event.preventDefault();
-    missNom.textContent = "Veuillez indiquer votre nom de famille svp";
-    missNom.style.color = "red";
-    codeRetour = false;
-
-  } else{
-    let firstName = document.getElementById("nom").value;
-    let lastName = document.getElementById("prenom").value;
-    let address = document.getElementById("adresse").value;
-    let city = document.getElementById("ville").value;
-    let email = document.getElementById("mail").value;
-  
-    // Objet contact à envoyer au serveur
-  
-    const contact = {
-      firstName: firstName,
-      lastName: lastName,
-      address: address,
-      city: city,
-      email: email,
-    };
-    console.log(contact);
-
-     // Tableau des id produit à envoyer au serveur
-    const products = panier.filter((item) => item._id).map((item) => item._id);
-  console.log(products);
-  }*/
-
-/*// si le champ prénom est vide
-  /// si le champs Nom est vide
-  if (nom.validity.valueMissing) {
-    event.preventDefault();
-    missNom.textContent = "Veuillez indiquer votre nom de famille svp";
-    missNom.style.color = "red";
-    codeRetour = false;
-  }
-  if (prenom.validity.valueMissing) {
-    event.preventDefault();
-    missPrenom.textContent = "Veuillez indiquer votre prénom svp";
-    missPrenom.style.color = "red";
-    codeRetour = false;
-  }
-  /// si le champ adresse est vide
-  if (adresse.validity.valueMissing) {
-    event.preventDefault();
-    missAdresse.textContent = "Veuillez indiquer votre adresse svp";
-    missAdresse.style.color = "red";
-    codeRetour = false;
-  }
-  /// si le champ ville est vide
-  if (ville.validity.valueMissing) {
-    event.preventDefault();
-    missVille.textContent = "Veuillez indiquer votre ville et code postal svp";
-    missVille.style.color = "red";
-    codeRetour = false;
-  }
-  /// si le champ mail est vide
-  if (mail.validity.valueMissing) {
-    event.preventDefault();
-    missMail.textContent = "Veuillez indiquer votre mail svp";
-    missMail.style.color = "red";
-    codeRetour = false;
-  } /*else {
-    // envoi au serveur les objets contact et product
-    fetch("http://localhost:3000/api/teddies/order", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        contact,
-        products,
-      }),
-    }).then((response) => response.json());
-    console.log(response);
-    /// enregistrement dans le localStorage
-    localStorage.setItem("contact", JSON.stringify(data));
-    localStorage.setItem("products", JSON.stringify(data));
-  } 
-}*/
